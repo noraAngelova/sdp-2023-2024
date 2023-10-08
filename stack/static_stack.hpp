2a7eb8205191fa428c2d09df8d05eddbc26c17a4
noraAngelova <73758640+noraAngelova@users.noreply.github.com>
2023-10-08 12:44:40 +0300

    Create static_stack.hpp

---
+++ stack/static_stack.hpp
@@ -0,0 +1,67 @@
+#ifndef _STATIC_STACK_HPP
+#define _STATIC_STACK_HPP
+
+#include <iostream>
+const unsigned MAX_SIZE = 1024;
+
+template <typename T>
+class Stack {
+private:
+    T elements[MAX_SIZE];  // Вътрешно представяне
+    int topIndex;          // Индекс на върха на стека
+
+public:
+    Stack();               // Създаване на празен стек
+
+    bool full() const;     // Помощна функция за запълненост
+    bool empty() const;    // Проверка дали стек е празен
+    void push(T const& x); // Включване на елемент
+    void pop();            // Изключване на елемент
+    T top() const;         // Достъп до върха на стека
+};
+
+// Конструктор за създаване на празен стек
+template <typename T>
+Stack<T>::Stack() : topIndex(-1) {}
+
+// Проверка дали стекът е празен - O(1)
+template <typename T>
+bool Stack<T>::empty() const {
+	return topIndex == -1;
+}
+
+// Проверка дали стекът е пълен - O(1)
+template <typename T>
+bool Stack<T>::full() const {
+	return topIndex == MAX_SIZE - 1;
+}
+
+// Извличане на върха на стека - O(1)
+template <typename T>
+T Stack<T>::top() const {
+    if (empty()) {
+        throw std::runtime_error("You can not get the top element of an empty stack!");
+    }
+
+    return elements[topIndex];
+}
+
+// Добавяне на елемент на върха стека - O(1)
+template <typename T>
+void Stack<T>::push(T const& x) {
+    if (full()) {
+        throw std::runtime_error("Stack is full!");
+    }
+    
+    elements[++topIndex] = x;
+}
+
+// Изтриване на елемента на върха на стека - O(1)
+template <typename T>
+void Stack<T>::pop() {
+    if (empty()) {
+        throw std::runtime_error("You can not delete the top element of an empty stack!");
+    }
+    topIndex--;
+}
+#endif
